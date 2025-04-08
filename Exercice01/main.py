c48bd23d82b2db84633d903c98e1a71c15f918b4
Charles DZADU <dzaducharles@gmail.com>
2025-04-08 15:43:28 +0000

    Exo 1. Add personalized greeting feature in main.py

--- Exercice01/main.py
+++ Exercice01/main.py
@@ -1,2 +1,11 @@
-name = input("Entrez votre nom : ")
-age = int(input("Entrez votre âge : "))
+# Programme qui affiche un message personnalisé avec f-string
+
+# Demander le nom de l'utilisateur
+nom = input("Entrez votre nom : ")
+
+# Demander l'âge de l'utilisateur
+age = input("Entrez votre âge : ")
+
+# Afficher le message personnalisé en utilisant f-string
+message = f"Bonjour, je m'appelle {nom} et j'ai {age} ans."
+print(message)
